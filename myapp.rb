2c9f2d440f64905944a58ea9d9304c0f95223ffa
Serrano Mark <JP11686@Siera20170308-784f43854f34.local>
2017-05-19 20:11:20 +0900

    heroku

--- myapp.rb
+++ myapp.rb
@@ -73,8 +73,9 @@ post '/callback' do
 
         response = HTTParty.post("https://graph.facebook.com/646906422185940/photos?url=http://maymm-photoshare.herokuapp.com/images/image_#{event.message['id']}.jpg", 
           :headers => headers
-	)
-	end
+        )
+	     end
+     end
 
   "OK"
 end
